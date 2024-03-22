d75a766614558164b676fba8879d18cb81f6d36d
Shreya Chikatmarla <131341300+shreyachika@users.noreply.github.com>
2024-03-21 17:04:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ st.write('---')
 with st.container():
   selected = option_menu(
     menu_title = None,
-    options = ['About', 'Projects', 'Contact']
+    options = ['About', 'Projects', 'Contact'],
     icons = ['person', 'code-slash', 'chat-left-text-fill']
   )
